47c8c05883389e140c3de184b18a486678aa3fff
szilard-molnar <mszilard86@gmail.com>
2020-05-19 13:26:18 -0400

    fetching data from api, basic style added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = { advice: '' }
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+            const { advice } = response.data.slip
+            console.log(advice);
+
+            this.setState({ advice });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        const { advice } = this.state;
+
+        return(
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
